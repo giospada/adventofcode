6eb67440eaf0eb9ddabec594185bb7ebd168a41a
Gio Spadaccini <giospadaccini74@gmail.com>
2021-10-13 13:23:35 +0200

    add 2 of day 4

--- 2020/D4/2.cpp
+++ 2020/D4/2.cpp
@@ -1,58 +1,183 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
-map<string,int> all={
-    {"byr",0},{"iyr",1},{"eyr",2},
-    {"hgt",3},{"hcl",4},{"ecl",5},{"pid",6}};
+class AttributoPasseggero{
+    public:
+        AttributoPasseggero(bool req,bool (*fun)(string)): required(req) {}
+        bool required;
+        bool (*check)(string);
+
+};
 
 
+class Passeggero{
 
+    public:
+        const static char SEPARATOR_KEYVALUE=':';
+        const static string SEPARATOR;
+        const static map<string,AttributoPasseggero> all;
 
-void check(string v,bitset<7> &bi){
-    auto it=all.find(v.substr(0,3));
-    if(it!= all.end()){
-        //0-2 :prefix
-        //3 :separator ':'
-        //value
-        switch(it->second){
-            case 0:
-                int val=stoi(v.substr(3,v.size()-4));
-                bi[0]|= (val>=1920 and val<=2002);
-                break;
-            case 1:
-                bi[0]|= (val>=1920 and val<=2002);
-                break;
+        map<string,string> values;
+        void readFromString(string input);
+        bool checkFiled() const;
+        bool checkValidFiled() const;
+        void clear();
+};
 
+const map<string, AttributoPasseggero> Passeggero::all = {
+    {"byr", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    try
+                                    {
+                                        int num = stoi(input);
+                                        return 1920 <= num and num <= 2002;
+                                    }
+                                    catch (...)
+                                    {
+                                        return false;
+                                    }
+                                })},
+    {"iyr", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    try
+                                    {
+                                        int num = stoi(input);
+                                        return 2010 <= num and num <= 2020;
+                                    }
+                                    catch (...)
+                                    {
+                                        return false;
+                                    }
+                                })},
+    {"eyr", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    try
+                                    {
+                                        int num = stoi(input);
+                                        return 2020 <= num and num <= 2030;
+                                    }
+                                    catch (...)
+                                    {
+                                        return false;
+                                    }
+                                })},
+    {"hgt", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    vector<tuple<string, int, int>> v = {make_tuple("cm", 1, 1), make_tuple("in", 1, 1)};
+                                    for (auto i : v)
+                                    {
+                                        size_t pos = input.find_first_of(get<0>(i));
+                                        if (pos != string::npos)
+                                        {
+                                        }
+                                    }
+                                })},
+    {"hcl", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    regex exp("\\#[a-f0-9]{6}");
+                                    return regex_match(input, exp);
+                                })},
+    {"ecl", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    vector<string> s = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+                                    for (auto i : s)
+                                        if (i == input)
+                                            return true;
+                                    return false;
+                                })},
+    {"pid", AttributoPasseggero(1,
+                                [](string input) -> bool
+                                {
+                                    regex exp("(\\d)+");
+                                    auto c = regex_match(input, exp);
+                                })},
+    {"cid", AttributoPasseggero(0,
+                                [](string input) -> bool
+                                {
+                                    return true;
+                                })}};
+
+const string Passeggero::SEPARATOR = " \n";
+
+void Passeggero::clear()
+{
+    values.clear();
+}
+
+bool Passeggero::checkValidFiled() const
+{
+    if (!checkFiled())
+        return false;
+
+    for (map<string, AttributoPasseggero>::const_iterator i = all.begin(); i != all.end(); i++)
+    {
+        if (values.count(i->first) == 1 and i->second.required == 1)
+        {
+            bool tempcheck = i->second.check(values.at(i->first));
+            if (!tempcheck)
+                return false;
         }
+    }
 
+    return true;
+}
+
+bool Passeggero::checkFiled() const
+{
+    for (map<string, AttributoPasseggero>::const_iterator i = all.begin(); i != all.end(); i++)
+    {
+        if (values.count(i->first) == 0 and i->second.required == 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Passeggero::readFromString(string input)
+{
+    input += "\n";
+    size_t found = input.find_first_of(SEPARATOR);
+    int startFrom = 0;
+    while (found != string::npos)
+    {
+        size_t breakPosition = input.find_first_of(SEPARATOR_KEYVALUE, startFrom);
+        if (breakPosition < found)
+        {
+            string key = input.substr(startFrom, breakPosition - startFrom);
+            string value = input.substr(breakPosition + 1, found - breakPosition - 1);
+            if (all.count(key) > 0)
+            {
+                values.insert({key, value});
+            }
+        }
+        startFrom = found + 1;
+        found = input.find_first_of(SEPARATOR, found + 1);
     }
 }
 
-
-vector<int> st;
-
-int main(){
-    int sol=0;
+int main()
+{
+    int sol = 0;
     string s;
-    bitset<7> bi;
-    while(getline(cin,s)){
-        if(s==""){
-            if(bi.all())
+    Passeggero passeggero;
+    while (getline(cin, s))
+    {
+        if (s == "")
+        {
+            if (passeggero.checkFiled())
                 sol++;
-            bi.reset();
+            passeggero.clear();
         }
-
-        int lsp=0; 
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                check(s.substr(lsp,i-lsp),bi);
-                lsp=i+1;
-            }
-        }
-        check(s.substr(lsp,s.size()-1),bi);
+        passeggero.readFromString(s);
     }
 
-    printf("%d\n",sol);
+    printf("%d\n", sol);
 }
