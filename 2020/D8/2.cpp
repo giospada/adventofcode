ffed39fda3c86e713dff5e7b4572f550f51de08c
Gio Spadaccini <giospadaccini74@gmail.com>
2021-10-23 18:13:37 +0200

    proa

--- 2020/D8/2.cpp
+++ 2020/D8/2.cpp
@@ -9,7 +9,31 @@ using namespace std;
 
 vtc<ay<int,2>> inp;
 vtc<bool> done;
+vtc<int> from;
+vtc<vtc<ay<int,2>>> v;
 
+//find next trova il prossimo jump o nop da una determinata posizione
+int findNext(int from){
+    for(int i=from;i<inp.size();i++)
+        if(inp[i][0]!=2)return i;
+    return -1;
+}
+
+
+int dfs(int c,int changed_node,int cost){
+    if(done[c])return -1;
+    done[c]=1;
+    for(auto i:v[c]){
+        if(i[0]==-1 and cost+i[1]==1){
+            return ((i[1]==1)?c:changed_node);
+        }else if(i[1]+cost<2){
+            int r=dfs(i[0],(i[1]==1?c:changed_node),cost+i[1]);
+            if(r!=-1)return r;
+        }
+    }
+    done[c]=0;
+    return -1;
+}
 
 int main(){
     map<string,int> ins={
@@ -32,12 +56,44 @@ int main(){
         }
     }
 
+    v.resize(inp.size());
+    rep(i,inp.size()){
+        ay<int,2> tmp=inp[i];
+        if(tmp[0]==2)continue;
+        int jumpI=findNext(i+(tmp[1]));
+        int nopI=findNext(i+1);
+        v[i].push_back({nopI,tmp[0]});
+        if(jumpI!=i) 
+            v[i].push_back({jumpI,1-tmp[0]});
+    }
 
-    //emulte mashine
+    /**
+    priority_queue<ay<int,2>> pq;
+    pq.push({0,findNext(0)});
+    int last=0;
+    while(!pq.empty()){
+        ay<int,2> tmp=pq.top();
+        pq.pop();
+        if(tmp[1]==-1){
+            break;
+        }
+        if(-tmp[0]>=done[tmp[1]])continue;
+        done[tmp[1]]=-tmp[0];
+        for(auto t:v[tmp[1]]){
+            if(t[0]==-1)
+                pq.push({-t[1]+tmp[0],t[0]});
+            else if(done[t[0]]>t[1]-tmp[0])
+                pq.push({-t[1]+tmp[0],t[0]});
+        }
+    }
+    dijikstra è implementato bene tranne trovare il nodo prima
+    */
+    int tochange=dfs(findNext(0),-1,0);
+    inp[tochange][0]=1-inp[tochange][0];
+
     int reg=0;
     int index=0;
-    while(not done[index]){
-        done[index]=1;
+    while(index<inp.size()){
         switch(inp[index][0]){
             case 1:
                 index+=inp[index][1]-1;
@@ -50,4 +106,5 @@ int main(){
         index++;
     }
     printf("%d",reg);
+
 }
